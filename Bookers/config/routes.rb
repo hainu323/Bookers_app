7a37eef019b20a1a0367c86cf35a2bb0c510191e
hainu323 <maxhainu@gmail.com>
2021-01-30 14:18:42 +0000

    update

--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :bookers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  root :to => 'books#top'
+    get 'books' =>'books#index'
+    post 'books' => 'books#create'
+    get 'books/:id' => 'books#show', as: 'book'
+    get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+    patch 'books/:id' => 'books#update', as: 'update_book'
+    put 'books/:id' => 'books#update'
+    delete 'books/:id' => 'books#destroy'
+ end
